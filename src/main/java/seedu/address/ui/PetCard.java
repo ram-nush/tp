524fc3cf316197d53eb168ee974d8f7c6f4beb6b
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-03-14 14:03:41 +0800

    Add PetCard.java

---
+++ src/main/java/seedu/address/ui/PetCard.java
@@ -0,0 +1,42 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.person.Pet;
+
+/**
+ * An UI component that displays information of a {@code Person}.
+ */
+public class PetCard extends UiPart<Region> {
+
+    private static final String FXML = "PetListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Pet pet;
+
+    @FXML
+    private HBox petCardPane;
+    @FXML
+    private Label petName;
+    @FXML
+    private Label id;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public PetCard(Pet pet, int displayedIndex) {
+        super(FXML);
+        this.pet = pet;
+        id.setText(displayedIndex + ". ");
+        petName.setText(pet.getName().fullName);
+    }
+}
